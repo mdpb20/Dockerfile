4abe1c6f8a1700548a777f7c5e2102beb84d7e4f
mdpb20 <mdpb20@gmail.com>
2025-05-20 19:54:22 +0200

    Update db.php

--- wp-content/db.php
+++ wp-content/db.php
@@ -9,30 +9,43 @@ Author URI: http://www.hawkix.net
 License: GPLv2 or newer.
 */
 
-if( !defined('PG4WP_ROOT'))
-{
-// You can choose the driver to load here
-define('DB_DRIVER', 'pgsql'); // 'pgsql' or 'mysql' are supported for now
+if ( ! defined('PG4WP_ROOT') ) {
 
-// Set this to 'true' and check that `pg4wp` is writable if you want debug logs to be written
-define( 'PG4WP_DEBUG', false);
-// If you just want to log queries that generate errors, leave PG4WP_DEBUG to "false"
-// and set this to true
-define( 'PG4WP_LOG_ERRORS', true);
+  // Sólo definimos el driver si no existe
+  if ( ! defined('DB_DRIVER') ) {
+    define('DB_DRIVER', 'pgsql'); // 'pgsql' or 'mysql'
+  }
 
-// If you want to allow insecure configuration (from the author point of view) to work with PG4WP,
-// change this to true
-define( 'PG4WP_INSECURE', false);
+  // Debug logs
+  if ( ! defined('PG4WP_DEBUG') ) {
+    define('PG4WP_DEBUG', false);
+  }
+  if ( ! defined('PG4WP_LOG_ERRORS') ) {
+    define('PG4WP_LOG_ERRORS', true);
+  }
 
-define( 'PG4WP_SCHEMA', 'public');
+  // Configuración insegura
+  if ( ! defined('PG4WP_INSECURE') ) {
+    define('PG4WP_INSECURE', false);
+  }
 
-// This defines the directory where PG4WP files are loaded from
-//   2 places checked : wp-content and wp-content/plugins
-if( file_exists( ABSPATH.'/wp-content/pg4wp'))
-	define( 'PG4WP_ROOT', ABSPATH.'/wp-content/pg4wp');
-else
-	define( 'PG4WP_ROOT', ABSPATH.'/wp-content/plugins/pg4wp');
+  // Esquema por defecto
+  if ( ! defined('PG4WP_SCHEMA') ) {
+    define('PG4WP_SCHEMA', 'public');
+  }
 
-// Here happens all the magic
-require_once( PG4WP_ROOT.'/core.php');
-} // Protection against multiple loading
+  // Determina la carpeta raíz de PG4WP
+  if ( file_exists(ABSPATH . '/wp-content/pg4wp') ) {
+    if ( ! defined('PG4WP_ROOT') ) {
+      define('PG4WP_ROOT', ABSPATH . '/wp-content/pg4wp');
+    }
+  } else {
+    if ( ! defined('PG4WP_ROOT') ) {
+      define('PG4WP_ROOT', ABSPATH . '/wp-content/plugins/pg4wp');
+    }
+  }
+
+  // Carga el core
+  require_once PG4WP_ROOT . '/core.php';
+}
+
