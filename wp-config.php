60e2be821f3899a5acb39cce98510e1e9a37cf31
mdpb20 <mdpb20@gmail.com>
2025-05-20 22:55:54 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,32 +1,28 @@
 <?php
-// Deshabilita el driver MySQL nativo
+// Deshabilita el driver mysql nativo
 define('WP_USE_EXT_MYSQL', false);
-
-// Le dices a WP que use PG4WP
+// Le dice a WP que use PG4WP
 define('DB_DRIVER', 'pgsql');
 
-// Las credenciales y el host/puerto los toma de las env vars de Render
+// Credenciales desde env vars
 define('DB_NAME',     getenv('WORDPRESS_DB_NAME'));
 define('DB_USER',     getenv('WORDPRESS_DB_USER'));
 define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
 
-// host y puerto por separado
+// Host y puerto por separado
 define('DB_HOST',     getenv('WORDPRESS_DB_HOST'));
 define('DB_PORT',     getenv('WORDPRESS_DB_PORT'));
 
-// Charset y collate estándar
 define('DB_CHARSET',  'utf8');
 define('DB_COLLATE',  '');
 
-// Prefijo de tablas
 $table_prefix = 'wp_';
-
-// Debug de WP
 define('WP_DEBUG', false);
 
-/* ¡Eso es todo, deja de editar! */
-if ( !defined('ABSPATH') )
+if ( ! defined('ABSPATH') ) {
   define('ABSPATH', dirname(__FILE__) . '/');
-require_once( ABSPATH . 'wp-settings.php' );
+}
+require_once ABSPATH . 'wp-settings.php';
+
 
 
