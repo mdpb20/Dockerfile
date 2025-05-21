3e69a9409b927e6cfe9afa400e498419ed25473f
mdpb20 <mdpb20@gmail.com>
2025-05-21 11:52:39 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -2,12 +2,27 @@
 // ** Ajustes de PG4WP y Postgres ** //
 define('WP_USE_EXT_MYSQL', false);
 
-// ** Credenciales desde variables de entorno ** //
-define('DB_NAME',     getenv('WORDPRESS_DB_NAME'));
-define('DB_USER',     getenv('WORDPRESS_DB_USER'));
-define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
-define('DB_HOST',     getenv('WORDPRESS_DB_HOST'));
-define('DB_PORT',     getenv('WORDPRESS_DB_PORT'));
+// Intentamos primero leer la URL única de la base externa
+$database_url = getenv('DATABASE_URL');
+
+if ( $database_url ) {
+    // parseamos: postgresql://user:pass@host:port/dbname
+    $parts = parse_url( $database_url );
+    define('DB_NAME',     ltrim($parts['path'], '/') );
+    define('DB_USER',     $parts['user'] );
+    define('DB_PASSWORD', $parts['pass'] );
+    // DB_HOST SIN puerto, y DB_PORT aparte:
+    define('DB_HOST',     $parts['host'] );
+    define('DB_PORT',     $parts['port'] );
+} else {
+    // Fallback a las 5 vars por separado
+    define('DB_NAME',     getenv('WORDPRESS_DB_NAME'));
+    define('DB_USER',     getenv('WORDPRESS_DB_USER'));
+    define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
+    define('DB_HOST',     getenv('WORDPRESS_DB_HOST'));
+    define('DB_PORT',     getenv('WORDPRESS_DB_PORT'));
+}
+
 define('DB_CHARSET',  'utf8');
 define('DB_COLLATE',  '');
 
@@ -21,5 +36,6 @@ define('WP_DEBUG_DISPLAY', false);
 
 /* ¡Eso es todo! */
 if ( !defined('ABSPATH') )
-  define('ABSPATH', dirname(__FILE__) . '/');
+    define('ABSPATH', dirname(__FILE__) . '/');
 require_once( ABSPATH . 'wp-settings.php' );
+
